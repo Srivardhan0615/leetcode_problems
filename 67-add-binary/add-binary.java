7ccbdf77a042c68c89217a034c9a6716260c2549
Srivardhan0615 <srivardhanneela@gmail.com>
2026-02-18 14:01:03 +0530

    Time: 1 ms (99.85%) | Memory: 43.6 MB (56.48%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) {
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int c = 0;
+        
+        StringBuilder sb = new StringBuilder();
+
+        while(i >= 0 || j >= 0 || c == 1){
+            if(i >= 0){
+                c = c + a.charAt(i--) - '0';
+            }if(j >= 0){
+                c = c +b.charAt(j--) - '0';
+            }
+            sb.append(c % 2);
+            c = c/2;
+
+        }
+        sb.reverse();
+        return String.valueOf(sb);
+        
+    }
+}

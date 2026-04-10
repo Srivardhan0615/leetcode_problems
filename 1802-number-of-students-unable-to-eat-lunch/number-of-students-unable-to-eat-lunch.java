6f02cd03998e8b96cc39fcec0422c43b0f493bf1
Srivardhan0615 <srivardhanneela@gmail.com>
2026-04-10 10:33:39 +0530

    Time: 3 ms (15.32%) | Memory: 43.6 MB (15.44%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+
+        Queue<Integer> stu = new LinkedList<>();
+        Stack<Integer> food = new Stack<>();
+
+        for (int i : students) {
+            stu.offer(i);
+        }
+
+   
+        for (int i = sandwiches.length - 1; i >= 0; i--) {
+            food.push(sandwiches[i]);
+        }
+
+        int count = 0; 
+
+        while (!stu.isEmpty() && count < stu.size()) {
+
+            if (stu.peek().equals(food.peek())) {
+                stu.poll();
+                food.pop();
+                count = 0; 
+            } else {
+                stu.offer(stu.poll());
+                count++;
+            }
+        }
+
+        return stu.size(); 
+    }
+}
